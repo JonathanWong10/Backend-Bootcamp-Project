be0dbca7d7bab379b20e3d1a5fc5f1bdcbf38985
Jonathan Wong <jonathan.wfc@gmail.com>
2025-04-12 01:25:11 +0400

    add-ip-address

--- main.js
+++ main.js
@@ -66,6 +66,6 @@ app.get('', function(req, res){
 
 //start server
 var port = 3000;
-app.listen(port, function(){
+app.listen(port, '0.0.0.0', function(){
     console.log(`Server is listening on port ${port}`);
 });
